7b5408a2d5ef697d2b40b279bba94e5b6d524b5e
June <junemvp@foxmail.com>
2021-08-25 23:03:52 +0800

    最长无重复字符串，没有做完

--- app/src/main/java/com/june/leetcode/string/LongestStringWithoutRepeated.java
+++ app/src/main/java/com/june/leetcode/string/LongestStringWithoutRepeated.java
@@ -1,6 +1,8 @@
 package com.june.leetcode.string;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 3. 无重复字符的最长子串
@@ -14,7 +16,8 @@ public class LongestStringWithoutRepeated {
 //        System.out.println("bbbbb:" + lengthOfLongestSubstring("bbbbb"));
 //        System.out.println("pwwkew:" + lengthOfLongestSubstring("pwwkew"));
 //        System.out.println("asjdkasjvl:" + lengthOfLongestSubstring("asjdkasjvl"));
-        System.out.println("tmmzuxt:" + lengthOfLongestSubstring("tmmzuxt"));
+//        System.out.println("tmmzuxt:" + lengthOfLongestSubstring("tmmzuxt"));
+        System.out.println("abcabcbb:" + lengthOfLongestSubstring("abcabcbb"));
     }
 
     public static int lengthOfLongestSubstring(String s) {
@@ -50,5 +53,28 @@ public class LongestStringWithoutRepeated {
             }
         }
         return maxLength;
+    }
+
+
+    public static int stringLength(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
     }
 }
