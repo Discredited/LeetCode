935f73511243470031c53b9e72fc786d9229035a
June <junemvp@foxmail.com>
2021-08-26 22:28:18 +0800

    无重复最长字符串解法完善和最优解法对比

--- app/src/main/java/com/june/leetcode/string/LongestStringWithoutRepeated.java
+++ app/src/main/java/com/june/leetcode/string/LongestStringWithoutRepeated.java
@@ -1,6 +1,5 @@
 package com.june.leetcode.string;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,54 +7,57 @@ import java.util.Set;
  * 3. 无重复字符的最长子串
  * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
  * 2021年08月25日17:39:24
+ * <p>
+ * 最优解法：滑动窗口
  */
 public class LongestStringWithoutRepeated {
 
     public static void main(String[] args) {
-//        System.out.println("abcabcbb的最大长度:" + lengthOfLongestSubstring("abcabcbb"));
-//        System.out.println("bbbbb:" + lengthOfLongestSubstring("bbbbb"));
-//        System.out.println("pwwkew:" + lengthOfLongestSubstring("pwwkew"));
-//        System.out.println("asjdkasjvl:" + lengthOfLongestSubstring("asjdkasjvl"));
-//        System.out.println("tmmzuxt:" + lengthOfLongestSubstring("tmmzuxt"));
+        System.out.println("abcabcbb的最大长度:" + lengthOfLongestSubstring("abcabcbb"));
+        System.out.println("bbbbb:" + lengthOfLongestSubstring("bbbbb"));
+        System.out.println("pwwkew:" + lengthOfLongestSubstring("pwwkew"));
+        System.out.println("asjdkasjvl:" + lengthOfLongestSubstring("asjdkasjvl"));
+        System.out.println("tmmzuxt:" + lengthOfLongestSubstring("tmmzuxt"));
         System.out.println("abcabcbb:" + lengthOfLongestSubstring("abcabcbb"));
+        System.out.println("au:" + lengthOfLongestSubstring("au"));
     }
 
+    /**
+     * 我的解法
+     * 76 ms	38.9 MB
+     *
+     * 相较于官方解法，左指针向右移动一格，移除一个字符的优化
+     */
     public static int lengthOfLongestSubstring(String s) {
         if (null == s) return 0;
 
         if (s.length() <= 1) return s.length();
 
         int maxLength = 1;
-        int startPosition = 0;
+        int currentLength = 0;
+        int startPosition = -1; // 其实位置应该为字符串的左边界
 
-        HashMap<Character, Integer> map = new HashMap<>();
+        HashSet<Character> set = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
-            Character c = s.charAt(i);
-            if (map.containsKey(c)) {
-                int length = i - startPosition;
-                if (length >= maxLength) maxLength = length;
-                System.out.println("====    " + c + "    containsKey    length:" + length + "    maxLength:" + maxLength);
-
-                //查找到相同字符后需要更新起始位置
-                startPosition = map.get(c) + 1;
-                System.out.println("====    " + c + "    更新startPosition:" + startPosition);
-
-                map.put(c, i);
-            } else {
-                if (i == s.length() - 1) {
-                    int length = i - startPosition + 1;
-                    if (length >= maxLength) maxLength = length;
-                    System.out.println("====    " + c + "    not containsKey    最后一位    length:" + length + "    maxLength:" + maxLength);
-                } else {
-                    map.put(c, i);
-                    System.out.println("====     " + c + "    not containsKey    不是最后    map:" + map.size());
-                }
+            while (startPosition < s.length() - 1 && !set.contains(s.charAt(startPosition + 1))) {
+                set.add(s.charAt(startPosition + 1));
+                startPosition++;
+                currentLength++;
             }
+            maxLength = Math.max(maxLength, currentLength);
+            // 重置数据
+            startPosition = i;
+            currentLength = 0;
+            set.clear();
         }
         return maxLength;
     }
 
 
+    /**
+     * 官方标准解法
+     * 6 ms	38.4 MB
+     */
     public static int stringLength(String s) {
         // 哈希集合，记录每个字符是否出现过
         Set<Character> occ = new HashSet();
