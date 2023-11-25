063db45b3f1e40688e089220a1921640dbed041d
June <junemvp@foxmail.com>
2023-11-25 20:16:18 +0800

    feat:两个链表求和

---
+++ app/src/main/java/com/june/leetcode/linear/link/AddTwoNumbers2.kt
@@ -0,0 +1,17 @@
+package com.june.leetcode.linear.link
+
+/**
+ * 445. 两数相加 II
+ * https://leetcode.cn/problems/add-two-numbers-ii/?envType=daily-question&envId=2023-11-25
+ *
+ * 2023/11/25
+ * @author June
+ */
+class AddTwoNumbers2 {
+
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        // 首先想到的是翻转链表
+        // 然后执行两个链表的同时遍历，直到两个链表指针都为空
+
+    }
+}
